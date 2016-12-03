602e512ef6bfd72d32bc78d2a25a2f823cdf888a
Wes Turner <wturnerharris@gmail.com>
2016-12-03 18:49:55 -0500

    Change build to use reactjs

---
+++ components/Home.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+function Home() {
+  const data = {
+    "bride" : ['Jamela','Williams'],
+    "groom" : ['Wesley','Turner-Harris'],
+    "invite" : [
+      'invite you to',
+      'celebrate their marriage',
+      'saturday, june 10, 2017',
+      'at seven in the evening'
+    ],
+    "location" : [
+      'the attic rooftop',
+      'new york, ny'
+    ]
+  };
+
+  return (
+    <section id="SaveTheDate">
+      <p>
+        <span className="bride first">{data.bride[0]}</span><br/>
+        <span className="bride second">{data.bride[1]}</span><br/>
+        <span className="amp">&</span><br/>
+        <span className="groom first">{data.groom[0]}</span><br/>
+        <span className="groom second">{data.groom[1]}</span><br/>
+        <br/>
+        <span className="invite">{data.invite[0]}</span><br/>
+        <span className="invite">{data.invite[1]}</span><br/>
+        <span className="invite">{data.invite[2]}</span><br/>
+        <span className="invite">{data.invite[3]}</span><br/>
+        <br/>
+        <span className="location">{data.location[0]}</span><br/>
+        <span className="location">{data.location[1]}</span><br/>
+      </p>
+    </section>
+  );
+}
+
+export default Home;
---
+++ index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import 'current-input';
+
+import App from './components/App';
+import Home from './components/Home';
+import PageNotFound from './components/PageNotFound';
+import Dress from './components/Dress';
+import Resources from './components/Resources';
+
+
+const routes = (
+  <Route path="/" mapMenuTitle="Home" component={App}>
+    <IndexRoute component={Home} />
+
+    <Route path="dress" mapMenuTitle="Dress" component={Dress}/>
+    <Route path="resources" mapMenuTitle="Resources" component={Resources}/>
+    <Route path="*" mapMenuTitle="Page Not Found" component={PageNotFound} />
+  </Route>
+);
+//    <Route path="registry" mapMenuTitle="Registry" component={Registry}/>
+//    <Route path="see-hear-do" mapMenuTitle="See+Hear+Do" component={SeeHearDo}/>
+//    <Route path="next-day-brunch" mapMenuTitle="Next-day Brunch" component={NextDayBrunch}/>
+
+
+render(
+  <Router
+    history={browserHistory}
+    routes={routes}
+  />,
+  document.getElementById('root')
+);
